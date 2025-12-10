02c0703bfb2b2c6dc40dea6c518459bc186ffc20
9Moses <esselmoses12@gmail.com>
2025-12-10 16:37:24 +0000

    update for event driven using kafka

---
+++ order-service/src/main/java/com/programmingSpaceX/orderService/event/OrderPlaceEvent.java
@@ -0,0 +1,12 @@
+package com.programmingSpaceX.orderService.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlaceEvent {
+    private String orderNumber;
+}
--- order-service/src/main/java/com/programmingSpaceX/orderService/service/OrderService.java
+++ order-service/src/main/java/com/programmingSpaceX/orderService/service/OrderService.java
@@ -1,21 +1,24 @@
 package com.programmingSpaceX.orderService.service;
 
-import brave.propagation.CurrentTraceContext;
+
 import com.programmingSpaceX.orderService.dto.InventoryResponse;
 import com.programmingSpaceX.orderService.dto.OrderRequest;
 import com.programmingSpaceX.orderService.dto.OrderLineItemDTO;
+import com.programmingSpaceX.orderService.event.OrderPlaceEvent;
 import com.programmingSpaceX.orderService.model.OrderEntity;
 import com.programmingSpaceX.orderService.model.OrderLineItem;
 import com.programmingSpaceX.orderService.repository.OrderRepository;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.lang.reflect.Array;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -23,6 +26,7 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository repository;
@@ -31,8 +35,8 @@ public class OrderService {
     @Autowired
     private final Tracer tracer;
 
-    @Autowired
-    private CurrentTraceContext currentTraceContext;
+    private final KafkaTemplate<String, OrderPlaceEvent> KafkaTemplate;
+
 
     public String placeOrder(OrderRequest request){
         OrderEntity order = new OrderEntity();
@@ -55,23 +59,34 @@ public class OrderService {
         try (Tracer.SpanInScope ws = tracer.withSpan(inventoryServiceLookup.start())) {
 
             // Call Inventory service
-            InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
-                    .uri("http://inventoryService/api/inventory",
-                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build()
+                    .get()
+                    .uri(uriBuilder -> {
+                        uriBuilder.path("http://inventory-service/api/inventory");
+                        skuCodes.forEach(sku -> uriBuilder.queryParam("skuCode", sku));
+                        return uriBuilder.build();
+                    })
                     .retrieve()
                     .bodyToMono(InventoryResponse[].class)
+                    .timeout(Duration.ofSeconds(5)) // don't wait forever
+                    .onErrorResume(ex -> {
+                        // fallback if inventory service is down
+                        throw new IllegalStateException("Inventory service unavailable, please try again later", ex);
+                    })
                     .block();
+            System.out.println("Calling Inventory Service with SKUs: " + skuCodes);
 
-            boolean allProductIsInStock = Arrays.stream(inventoryResponsesArray)
+            assert inventoryResponseArray != null;
+            boolean allProductIsInStock = Arrays.stream(inventoryResponseArray)
                     .allMatch(InventoryResponse::isInStock);
 
             if (allProductIsInStock) {
                 repository.save(order);
+                KafkaTemplate.send("notificationTopic", new OrderPlaceEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
             }
-
-            return "Order Placed Successfully";
 
         } finally {
             inventoryServiceLookup.end();
